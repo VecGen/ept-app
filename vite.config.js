d17f388060d582445990dedb39717500a9a1517e
Rahul Talreja <Rahul.Talreja@morningstar.com>
2025-06-20 01:45:51 +0530

    updated vite config

--- vite.config.js
+++ vite.config.js
@@ -14,8 +14,9 @@ export default defineConfig(({ mode }) => ({
     port: 5173,
     proxy: {
       '/api': {
-        target: process.env.VITE_API_BASE_URL || 'http://localhost:8080',
-        changeOrigin: true
+        target: process.env.VITE_API_BASE_URL || 'https://mnwpivaen5.us-east-1.awsapprunner.com',
+        changeOrigin: true,
+        secure: true
       }
     }
   },
